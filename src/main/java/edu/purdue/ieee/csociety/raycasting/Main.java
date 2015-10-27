b73e124dcaf47c201504f6b00d0c41c0ee261de8
Vincent Zhang <vzhang@purdue.edu>
2015-10-27 11:59:39 -0400

    Initialize logging

--- src/main/java/edu/purdue/ieee/csociety/raycasting/Main.java
+++ src/main/java/edu/purdue/ieee/csociety/raycasting/Main.java
@@ -1,9 +1,16 @@
 package edu.purdue.ieee.csociety.raycasting;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static final Logger LOGGER = LogManager.getLogger("Raycaster");
 
+    public static void main(String[] args) {
+        Thread.currentThread().setName("main");
+        LOGGER.info("Starting...");
+        //  TODO
     }
 
 }
