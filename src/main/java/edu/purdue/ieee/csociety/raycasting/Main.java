c5f9f0d5ecaadb0846a6b0aa0908d60839de23c1
Vincent Zhang <vzhang@purdue.edu>
2015-10-27 12:24:22 -0400

    Implement basic main

--- src/main/java/edu/purdue/ieee/csociety/raycasting/Main.java
+++ src/main/java/edu/purdue/ieee/csociety/raycasting/Main.java
@@ -1,7 +1,9 @@
 package edu.purdue.ieee.csociety.raycasting;
 
+import edu.purdue.ieee.csociety.raycasting.util.SharedLibraryLoader;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.lwjgl.Sys;
 
 public class Main {
 
@@ -10,6 +12,23 @@ public class Main {
     public static void main(String[] args) {
         Thread.currentThread().setName("main");
         LOGGER.info("Starting...");
+        //  Load LWJGL native libs
+        SharedLibraryLoader.load();
+        Main main = new Main();
+        main.run();
+    }
+
+    public void run() {
+        LOGGER.info("LWJL version is {}", Sys.getVersion());
+
+        //  TODO
+    }
+
+    private void init() {
+        //  TODO
+    }
+
+    private void mainLoop() {
         //  TODO
     }
 
