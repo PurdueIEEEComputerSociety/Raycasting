eec61c9d10d01b278162f7d18cd03c34379c9250
Vincent Zhang <vzhang@purdue.edu>
2015-10-27 12:28:20 -0400

    Implement app run

--- src/main/java/edu/purdue/ieee/csociety/raycasting/Main.java
+++ src/main/java/edu/purdue/ieee/csociety/raycasting/Main.java
@@ -4,6 +4,10 @@ import edu.purdue.ieee.csociety.raycasting.util.SharedLibraryLoader;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.lwjgl.Sys;
+import org.lwjgl.glfw.GLFWErrorCallback;
+import org.lwjgl.glfw.GLFWKeyCallback;
+
+import static org.lwjgl.glfw.GLFW.glfwTerminate;
 
 public class Main {
 
@@ -11,17 +15,44 @@ public class Main {
 
     public static void main(String[] args) {
         Thread.currentThread().setName("main");
-        LOGGER.info("Starting...");
+        LOGGER.info("Loading libraries");
         //  Load LWJGL native libs
         SharedLibraryLoader.load();
         Main main = new Main();
         main.run();
     }
 
-    public void run() {
-        LOGGER.info("LWJL version is {}", Sys.getVersion());
+    /**
+     * Error handling callback
+     */
+    private GLFWErrorCallback errorCallback;
+    /**
+     * Input handling callback
+     */
+    private GLFWKeyCallback keyCallback;
+    /**
+     * Window handle
+     */
+    private long windowHandle;
 
-        //  TODO
+    public void run() {
+        LOGGER.info("Starting");
+        LOGGER.info("LWJL version is {}", Sys.getVersion());
+        try {
+            //  Inititalize our program
+            init();
+            //  Enter main loop
+            mainLoop();
+            //  Exiting the main loop means we're closing
+            finish();
+        } finally {
+            //  Clean up
+            glfwTerminate();
+            if (errorCallback != null) {
+                errorCallback.release();
+            }
+        }
+        LOGGER.info("Stopped");
     }
 
     private void init() {
@@ -30,6 +61,10 @@ public class Main {
 
     private void mainLoop() {
         //  TODO
+    }
+
+    private void finish() {
+
     }
 
 }
